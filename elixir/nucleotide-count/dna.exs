01b47a3fbb692ee191b4482f8ed450d8221d2771
megano0body <fernando.rbaroni@gmail.com>
2016-07-26 01:33:09 -0300

    Finished elixir nucleotide exercise

--- elixir/nucleotide-count/dna.exs
+++ elixir/nucleotide-count/dna.exs
@@ -1,5 +1,6 @@
 defmodule DNA do
   @nucleotides [?A, ?C, ?G, ?T]
+  @empty_histogram %{?A => 0, ?T => 0, ?C => 0, ?G => 0}
 
   @doc """
   Counts individual nucleotides in a DNA strand.
@@ -14,10 +15,12 @@ defmodule DNA do
   """
   @spec count([char], char) :: non_neg_integer
   def count(strand, nucleotide) do
-
+    validate_nucleotide(nucleotide)
+    strand
+    |> histogram
+    |> Map.get(nucleotide)
   end
-
-
+  
   @doc """
   Returns a summary of counts by nucleotide.
 
@@ -28,6 +31,19 @@ defmodule DNA do
   """
   @spec histogram([char]) :: map
   def histogram(strand) do
+    strand
+    |> calculate_histogram(@empty_histogram)
+  end
 
+  defp calculate_histogram([], hist), do: hist
+  defp calculate_histogram([nucleotide | tail], hist) do
+    validate_nucleotide(nucleotide)
+    updated_histogram = Map.update! hist, nucleotide, &(&1 + 1)
+    calculate_histogram(tail, updated_histogram)
+  end
+
+  defp validate_nucleotide(nucleotide) do
+    unless Enum.member?(@nucleotides, nucleotide), do:
+      raise ArgumentError, message: "Invalid nucleotide"
   end
 end
