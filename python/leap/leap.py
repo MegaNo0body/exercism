d2285b9733a6be1f6125bb90a16f2b1ad715d694
Fernando Rodrigues Baroni <fernando@baroni.tech>
2017-02-07 14:56:57 -0200

    Python - Leap

---
+++ python/leap/leap.py
@@ -0,0 +1,11 @@
+def is_leap_year(year):
+    """
+    on every year that is evenly divisible by 4
+      except every year that is evenly divisible by 100
+        unless the year is also evenly divisible by 400
+    """
+    if (year % 4) == 0:
+        if not (((year % 400) != 0) and ((year % 100) == 0)):
+            return True
+    return False
+
